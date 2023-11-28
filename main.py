070d77e45ae834fed475982ee3202cd4240e523e
dombom123 <dominik.boehm@gmx.de>
2023-11-28 11:39:41 +0100

    clean up and add header img

--- main.py
+++ main.py
@@ -216,6 +216,7 @@ def main():
         This app generates a **reaction video** from a video uploaded by the user.
         """
     )
+    st.image("header_img.jpg")
 
 
     # Use file_uploader to allow the user to upload a video
