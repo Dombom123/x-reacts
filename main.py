a77a3fa98a51c6250941ea1110c5dcdf6915ad5b
Dombom123 <dominik.boehm@gmx.de>
2023-12-08 18:42:10 +0100

    Delete unnecessary files

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import base64
 import time
 import requests
 from openai import OpenAI
-from moviepy.editor import VideoFileClip, CompositeVideoClip, AudioFileClip, CompositeAudioClip
+from moviepy.editor import VideoFileClip, CompositeVideoClip, AudioFileClip, concatenate_videoclips
 
-from moviepy.audio.fx.all import volumex
-from moviepy.video.compositing.concatenate import concatenate_videoclips
 import numpy as np
-
 import streamlit as st
 from PIL import Image
+
 def read_frames_from_video(path_to_video):
     # Check if the video file exists
     if not os.path.exists(path_to_video):
@@ -29,13 +27,13 @@ def read_frames_from_video(path_to_video):
     video.release()
     return base64Frames
 
-def generate_text_from_frames(prompt, path_to_video, base64Frames):
+def generate_transcript(path_to_video):
     api_key = st.secrets["openai"]["OPENAI_API_KEY"]
     client = OpenAI(api_key=api_key)
     # Get the audio from the video
     audio = AudioFileClip(path_to_video)
-    audio.write_audiofile("speech.mp3")
-    audio_file = open("speech.mp3", "rb")
+    audio.write_audiofile("data/speech.mp3")
+    audio_file = open("data/speech.mp3", "rb")
     transcript = client.audio.transcriptions.create(
     model="whisper-1", 
     file=audio_file
@@ -46,18 +44,26 @@ def generate_text_from_frames(prompt, path_to_video, base64Frames):
 
     st.subheader("Transcript")
     st.write(text_content)
-    
+
+    return text_content
+
+def generate_text_from_video(path_to_video):
+    api_key = st.secrets["openai"]["OPENAI_API_KEY"]
+    client = OpenAI(api_key=api_key)
+    base64Frames = read_frames_from_video(path_to_video)
+    transcript = generate_transcript(path_to_video)
     video_length_in_seconds = len(base64Frames) / 30
     st.write(f"Video Length: {video_length_in_seconds} seconds")
-    max_frames = 20
+    max_frames = 2
     frame_divider = len(base64Frames) // max_frames
-
+    st.write(f"Number of frames: {len(base64Frames)}")
+    st.write(f"Number of frames to use: {max_frames}")
     PROMPT_MESSAGES = [
         {
             "role": "user",
             "content": [
-                f"""{prompt}. Das Video ist {video_length_in_seconds} Sekunden lang.
-                Das ist das Audiotranskript des Videos: {text_content}. Das sind Frames aus dem Video:""",
+                f"""Bitte generiere eine detaillierte Zusammenfassung von Informationen über ein Tik Tok Video. Du kannst auch Details hinzufügen bei denen du dir unsicher bist, aber bleibe Konkret und stelle Vermutungen als Aussage dar. Das Video ist {video_length_in_seconds} Sekunden lang.
+                Das ist das Audiotranskript des Videos: {transcript}. Das sind Frames aus dem Video:""",
                 
                 *map(lambda x: {"image": x, "resize": 768}, base64Frames[0::frame_divider]),
             ],
@@ -66,10 +72,37 @@ def generate_text_from_frames(prompt, path_to_video, base64Frames):
     params = {
         "model": "gpt-4-vision-preview",
         "messages": PROMPT_MESSAGES,
+        "max_tokens": 500,
+    }
+    result = client.chat.completions.create(**params)
+    st.subheader("GPT Vision Text")
+    st.write(result.choices[0].message.content)
+    return result.choices[0].message.content
+
+def generate_voiceover_from_text(prompt, text):
+    api_key = st.secrets["openai"]["OPENAI_API_KEY"]
+    client = OpenAI(api_key=api_key)
+
+    PROMPT_MESSAGES = [
+        {
+            "role": "system",
+            "content": f"{prompt}"
+        },
+        {
+            "role": "user",
+            "content": f"Hier ist die Zusammenfassung des Videos: {text}."
+        },
+    ]
+    params = {
+        "model": "gpt-4",
+        "messages": PROMPT_MESSAGES,
         "max_tokens": 100,
     }
     result = client.chat.completions.create(**params)
+    st.subheader("Voiceover Text")
+    st.write(result.choices[0].message.content)
     return result.choices[0].message.content
+
 
 def generate_audio_from_text(text):
     api_key = st.secrets["openai"]["OPENAI_API_KEY"]
@@ -90,12 +123,12 @@ def generate_audio_from_text(text):
         audio += chunk
 
     # Write audio bytes to a file
-    with open('audio.mp3', 'wb') as f:
+    with open('data/audio.mp3', 'wb') as f:
         f.write(audio)
         f.close()
     print("Audio saved to audio.mp3")
     st.write("Audio saved to audio.mp3")
-    filepath = 'audio.mp3'
+    filepath = 'data/audio.mp3'
 
     return filepath
 
@@ -126,7 +159,6 @@ def generate_video_from_audio(audio_url):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-
         "authorization": authorization
     }
 
@@ -150,7 +182,7 @@ def generate_video_from_audio(audio_url):
         time.sleep(5)  # wait for 5 seconds before retrying
 
     # Download from url
-    path = "avatar.mp4"
+    path = "data/avatar.mp4"
     response = requests.get(url)
     with open(path, "wb") as f:
         f.write(response.content)
@@ -205,72 +237,86 @@ def assemble_video(gen_path, original_video_path, audio_path):
     composite_video = composite_video.set_audio(audio)
 
     # Write the final video to a file
-    composite_video.write_videofile('final.mp4')
+    composite_video.write_videofile('data/final.mp4')
+
+def generate_text_for_video(uploaded_file, prompt, progress_bar):
+    with open('data/temp_video.mp4', 'wb') as f:
+        f.write(uploaded_file.read())
+    progress_bar.progress(10)
+
+    path_to_video = 'data/temp_video.mp4'
+    progress_bar.progress(15)
+
+    vision_text = generate_text_from_video(path_to_video)
+    voiceover_text = generate_voiceover_from_text(prompt, vision_text)
+
+    progress_bar.progress(20)
+
+    return voiceover_text, path_to_video
+
+def process_video_with_text(text, path_to_video, progress_bar):
+    audio_path = generate_audio_from_text(text)
+    progress_bar.progress(30)
+    # Wait for 5 seconds
+    time.sleep(5)
+
+    audio_url = upload_audio_to_did(audio_path)
+    progress_bar.progress(50)
+
+    gen_video_path = generate_video_from_audio(audio_url)
+    progress_bar.progress(80)
+
+    assemble_video(gen_video_path, path_to_video, audio_path)
+    progress_bar.progress(100)
+
+    return open('data/final.mp4', 'rb')
+
+def display_video(video_file):
+    st.video(video_file)
 
 def main():
-    # Initialize Streamlit
     st.set_page_config(page_title="X-Reacts", page_icon="🎥")
-
     st.title("X-Reacts Video Generation 🎥")
-    st.markdown(
-        """
-        This app generates **a reaction video** from a video uploaded by the user.
-        """
-    )
-    st.image("header.png")
+    st.image("data/header.png")
 
-
-    # drop down menu
     st.subheader("Step 1: Select a prompt")
     prompts = {
-        "Default": """Wir spielen ein Rollenspiel. Sag einen Satz zu den Bildern als wärst du Satan, ein fieser und witziger Instagramteufel. """,
-        "Custom": """Wir spielen ein Rollenspiel. Sag einen Satz zu den Bildern als wärst du Satan, ein fieser und witziger Instagramteufel. """,
+        "Default": "Du schreibst witzige Kommentare zu Tik Tok Videos in weniger als 5 Sätzen.",
+        "Custom": "Du schreibst witzige Kommentare zu Tik Tok Videos in weniger als 5 Sätzen."
     }
-    default_prompt = """Wir spielen ein Rollenspiel. Sag einen Satz zu den Bildern als wärst du Satan, ein fieser und witziger Instagramteufel. """
+    prompt_selection = st.selectbox("Prompt Selection:", list(prompts.keys()))
+    if prompt_selection == "Custom":
+        prompt = st.text_area('Enter your prompt', prompts[prompt_selection])
+    else:
+        prompt = prompts[prompt_selection]
 
-    prompt = st.selectbox("Prompt Selection:", list(prompts.keys()))
-    if prompt == "Custom":
-        prompt = st.text_area('Enter your prompt', default_prompt)
-
-
-    
     st.subheader("Step 2: Upload a video")
-    uploaded_file = st.file_uploader("Choose a video file:", type=['mp4'], key='video', help='Upload a video file to get a reaction video')
+    uploaded_file = st.file_uploader("Choose a video file:", type=['mp4'], key='video')
+    # Toggle for activating video generation
+    is_generation_activated = st.toggle('Activate Video Generation')
 
-    if st.button('Start generating') and uploaded_file is not None:
-        with open('temp_video.mp4', 'wb') as f:
+    # Button to start generating
+    if st.button('Start generating'):
+        # Check if the toggle is activated and a file is uploaded
+        if is_generation_activated and uploaded_file is not None:
             bar = st.progress(5)
-            f.write(uploaded_file.read())
-        with st.spinner('Processing the video...'):
-            path_to_video = 'temp_video.mp4'
-            bar.progress(10)
-            base64Frames = read_frames_from_video(path_to_video)
-            bar.progress(15)
-            text = generate_text_from_frames(prompt, path_to_video, base64Frames)
-            bar.progress(20)
-            st.subheader("Voiceover Text")
-            st.write(text)
-            audio_path = generate_audio_from_text(text) 
-            bar.progress(30)          
-            audio_url = upload_audio_to_did(audio_path)
-            bar.progress(50)
-            gen_video_path = generate_video_from_audio(audio_url)
-            bar.progress(80)
-            assemble_video(gen_video_path, path_to_video, audio_path)
-            bar.progress(100)
-            st.success('Done!')
-            st.subheader("Generated Video")
-            video_file = open('final.mp4', 'rb')
+            with st.spinner('Generating text...'):
+                text, path_to_video = generate_text_for_video(uploaded_file, prompt, bar)
+                st.success('Text generated!')
 
-            video_bytes = video_file.read()
-            st.markdown(
-                f"""
-                <video width="320" height="240" controls>
-                    <source src="data:video/mp4;base64,{base64.b64encode(video_bytes).decode()}" type="video/mp4">
-                </video>
-                """,
-                unsafe_allow_html=True,
-            )
+            with st.spinner('Processing the video...'):
+                video_file = process_video_with_text(text, path_to_video, bar)
+                st.success('Done!')
+                st.subheader("Generated Video")
+                display_video(video_file)
+        elif not is_generation_activated and uploaded_file is not None:
+            bar = st.progress(5)
+            with st.spinner('Dev mode: Generating only text...'):
+                text, path_to_video = generate_text_for_video(uploaded_file, prompt, bar)
+                st.success('Text generated!')
+        else:
+            st.warning("Please upload a video to proceed.")
+
 
 if __name__ == "__main__":
     main()
